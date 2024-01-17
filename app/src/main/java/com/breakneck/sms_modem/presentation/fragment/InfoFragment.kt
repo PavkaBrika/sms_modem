115045c0832bc501d5cd2b9d6d461fb163d1e61f
PavkaBrika <pavlikbrichkin11@gmail.com>
2024-01-17 17:58:59 +0500

    added logic for info screen, reduced functionality

--- app/src/main/java/com/breakneck/sms_modem/presentation/fragment/InfoFragment.kt
+++ app/src/main/java/com/breakneck/sms_modem/presentation/fragment/InfoFragment.kt
@@ -1,11 +1,15 @@
 package com.breakneck.sms_modem.presentation.fragment
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.BuildCompat
 import androidx.fragment.app.Fragment
+import com.breakneck.sms_modem.R
 import com.breakneck.sms_modem.databinding.FragmentInfoBinding
 
 class InfoFragment: Fragment() {
@@ -28,7 +32,20 @@ class InfoFragment: Fragment() {
         binding = FragmentInfoBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        val version = requireActivity().packageManager.getPackageInfo("com.breakneck.sms_modem", 0).versionName
 
+        binding.writeEmailLayout.setOnClickListener {
+            Intent(Intent.ACTION_SENDTO)
+                .apply {
+                    data = Uri.parse("mailto:pavlikbrichkin@yandex.ru")
+                    putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject, version))
+                }
+                .let {
+                    startActivity(it)
+                }
+        }
+
+        binding.appVersionTextView.text = getString(R.string.app_version, version)
 
         return view
     }
